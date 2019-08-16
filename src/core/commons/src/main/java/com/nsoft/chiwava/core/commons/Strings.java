f91f5071c63c444f037d2e1ff7965b183901129f
Mislav Milicevic <milicevic.mislav@nsoft.com>
2019-08-16 14:52:03 +0200

    Add Strings utility class
    Refs #2

---
+++ src/core/commons/src/main/java/com/nsoft/chiwava/core/commons/Strings.java
@@ -0,0 +1,153 @@
+package com.nsoft.chiwava.core.commons;
+
+import java.util.Objects;
+
+/**
+ * Utility class used for various {@link String} checks and manipulations
+ *
+ * @author Mislav Milicevic
+ * @since 2019-08-16
+ */
+public final class Strings {
+
+    private Strings() {
+    }
+
+    /**
+     * Capitalizes the first character of the input {@link String}
+     *
+     * @param input String to be capitalized
+     * @return input with first character capitalized
+     */
+    public static String upperCaseFirst(String input) {
+        return upperCaseFirst(input, 1);
+    }
+
+    /**
+     * Capitalizes the first {@code n} characters of the input {@link String}
+     *
+     * @param input String to be capitalized
+     * @param n number of characters to be capitalized
+     * @return input with {@code n} first characters capitalized
+     */
+    public static String upperCaseFirst(String input, int n) {
+        Objects.requireNonNull(input, "Input can't be null");
+
+        if (n < 1 || n > input.length()) {
+            throw new IllegalArgumentException(
+                    String.format("Invalid character index {found: %d, expected: >= 1 || %d <=}", n,
+                            input.length()));
+        }
+
+        return input.substring(0, n).toUpperCase() + input.substring(n);
+    }
+
+    /**
+     * Capitalizes the last character of the input {@link String}
+     *
+     * @param input String to be capitalized
+     * @return input with the last character capitalized
+     */
+    public static String upperCaseLast(String input) {
+        return upperCaseLast(input, 1);
+    }
+
+    /**
+     * Capitalizes the last {@code n} characters of the input {@link String}
+     *
+     * @param input String to be capitalized
+     * @param n number of characters to be capitalized
+     * @return input with {@code n} last characters capitalized
+     */
+    public static String upperCaseLast(String input, int n) {
+        Objects.requireNonNull(input, "Input can't be null");
+
+        if (n < 1 || n > input.length()) {
+            throw new IllegalArgumentException(
+                    String.format("Invalid character index {found: %d, expected: >= 1 || %d <=}", n,
+                            input.length()));
+        }
+
+        int substringIndex = input.length() - n;
+        return input.substring(0, substringIndex) + input.substring(substringIndex).toUpperCase();
+    }
+
+    /**
+     * Converts the first character of the input {@link String} to lowercase
+     *
+     * @param input String to be converted to lowercase
+     * @return input with first character converted to lowercase
+     */
+    public static String lowerCaseFirst(String input) {
+        return lowerCaseFirst(input, 1);
+    }
+
+    /**
+     * Converts the first {@code n} characters of the input {@link String} to lowercase
+     *
+     * @param input String to be converted to lowercase
+     * @param n number of characters to be converted to lowercase
+     * @return input with first {@code n} characters converted to lowercase
+     */
+    public static String lowerCaseFirst(String input, int n) {
+        Objects.requireNonNull(input, "Input can't be null");
+
+        if (n < 1 || n > input.length()) {
+            throw new IllegalArgumentException(
+                    String.format("Invalid character index {found: %d, expected: >= 1 || %d <=}", n,
+                            input.length()));
+        }
+
+        return input.substring(0, n).toLowerCase() + input.substring(n);
+    }
+
+    /**
+     * Converts the last character of the input {@link String} to lowercase
+     *
+     * @param input String to be converted to lowercase
+     * @return input with last character converted to lowercase
+     */
+    public static String lowerCaseLast(String input) {
+        return lowerCaseLast(input, 1);
+    }
+
+    /**
+     * Converts the last {@code n} characters of the input {@link String} to lowercase
+     *
+     * @param input String to be converted to lowercase
+     * @param n number of characters to be converted to lowercase
+     * @return input with last {@code n} characters converted to lowercase
+     */
+    public static String lowerCaseLast(String input, int n) {
+        Objects.requireNonNull(input, "Input can't be null");
+
+        if (n < 1 || n > input.length()) {
+            throw new IllegalArgumentException(
+                    String.format("Invalid character index {found: %d, expected: >= 1 || %d <=}", n,
+                            input.length()));
+        }
+
+        int substringIndex = input.length() - n;
+        return input.substring(0, substringIndex) + input.substring(substringIndex).toLowerCase();
+    }
+
+    /**
+     * Inserts a character into a {@link String} at a specified position
+     *
+     * @param input String to be manipulated
+     * @param c character to be inserted
+     * @param i index at which the character will be inserted
+     * @return input with inserted character at specified index
+     */
+    public static String insertCharacterAt(String input, char c, int i) {
+        Objects.requireNonNull(input, "Input can't be null");
+
+        if (i < 0 || i > input.length() - 1) {
+            throw new IllegalArgumentException(
+                    String.format("Invalid character index {found: %d, expected: >= 0 || %d <=}", i,
+                            input.length() - 1));
+        }
+
+        return input.substring(0, i) + c + input.substring(i);
+    }
+}
---
+++ src/core/commons/src/test/java/com/nsoft/chiwava/core/commons/unit/StringsTest.java
@@ -0,0 +1,147 @@
+package com.nsoft.chiwava.core.commons.unit;
+
+import com.nsoft.chiwava.core.commons.Strings;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class StringsTest {
+
+    @Test
+    public void shouldUpperCaseFirstCharacter() {
+        String result = Strings.upperCaseFirst("input");
+
+        Assert.assertEquals("Input", result);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void shouldNotUpperCaseFirstCharacter_NullInput() {
+        Strings.upperCaseFirst(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldNotUpperCaseFirstCharacter_InvalidIndexLower() {
+        Strings.upperCaseFirst("input", 0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldNotUpperCaseFirstCharacter_InvalidIndexHigher() {
+        Strings.upperCaseFirst("input", "input".length() + 1);
+    }
+
+    @Test
+    public void shouldUpperCaseFirstTwoCharacters() {
+        String result = Strings.upperCaseFirst("input", 2);
+
+        Assert.assertEquals("INput", result);
+    }
+
+    @Test
+    public void shouldUpperCaseLastCharacter() {
+        String result = Strings.upperCaseLast("input");
+
+        Assert.assertEquals("inpuT", result);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void shouldNotUpperCaseLastCharacter_NullInput() {
+        Strings.upperCaseLast(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldNotUpperCaseLastCharacter_InvalidIndexLower() {
+        Strings.upperCaseLast("input", 0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldNotUpperCaseLastCharacter_InvalidIndexUpper() {
+        Strings.upperCaseLast("input", "input".length() + 1);
+    }
+
+    @Test
+    public void shouldUpperCaseLastTwoCharacters() {
+        String result = Strings.upperCaseLast("input", 2);
+
+        Assert.assertEquals("inpUT", result);
+    }
+
+    @Test
+    public void shouldLowerCaseFirstCharacter() {
+        String result = Strings.lowerCaseFirst("INPUT");
+
+        Assert.assertEquals("iNPUT", result);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void shouldNotLowerCaseFirstCharacter_NullInput() {
+        Strings.lowerCaseFirst(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldNotLowerCaseFirstCharacter_InvalidIndexLower() {
+        Strings.lowerCaseFirst("INPUT", 0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldNotLowerCaseFirstCharacter_InvalidIndexHigher() {
+        Strings.lowerCaseFirst("INPUT", "INPUT".length() + 1);
+    }
+
+    @Test
+    public void shouldLowerCaseFirstTwoCharacters() {
+        String result = Strings.lowerCaseFirst("INPUT", 2);
+
+        Assert.assertEquals("inPUT", result);
+    }
+
+    @Test
+    public void shouldLowerCaseLastCharacter() {
+        String result = Strings.lowerCaseLast("INPUT");
+
+        Assert.assertEquals("INPUt", result);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void shouldNotLowerCaseLastCharacter_NullInput() {
+        Strings.lowerCaseLast(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldNotLowerCaseLastCharacter_InvalidIndexLower() {
+        Strings.lowerCaseLast("INPUT", 0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldNotLowerCaseLastCharacter_InvalidIndexUpper() {
+        Strings.lowerCaseLast("INPUT", "INPUT".length() + 1);
+    }
+
+    @Test
+    public void shouldLowerCaseLastTwoCharacters() {
+        String result = Strings.lowerCaseLast("INPUT", 2);
+
+        Assert.assertEquals("INPut", result);
+    }
+
+    @Test
+    public void shouldInsertCharacter() {
+        String result = Strings.insertCharacterAt("input", 'm', 1);
+
+        Assert.assertEquals("imnput", result);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void shouldNotInsertCharacter_NullInput() {
+        Strings.insertCharacterAt(null, 'm', 1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldNotInsertCharacter_InvalidIndexLower() {
+        Strings.insertCharacterAt("input", 'm', -1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldNotInsertCharacter_InvalidIndexUpper() {
+        Strings.insertCharacterAt("input", 'm', "input".length());
+
+    }
+}
