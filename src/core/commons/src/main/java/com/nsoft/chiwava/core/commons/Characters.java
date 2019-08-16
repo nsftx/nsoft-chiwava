200198c721419e83c618945a2799a0683497ef0d
Mislav Milicevic <milicevic.mislav@nsoft.com>
2019-08-16 11:09:10 +0200

    Extend Characters utility class
    Refs #2

--- src/core/commons/src/main/java/com/nsoft/chiwava/core/commons/Characters.java
+++ src/core/commons/src/main/java/com/nsoft/chiwava/core/commons/Characters.java
@@ -3,6 +3,9 @@ package com.nsoft.chiwava.core.commons;
 /**
  * Utility class used for various character checks and manipulations
  *
+ * Methods defined in this class are applicable to the ISO-8859-1 character set, characters outside
+ * of its specification are not guaranteed to behave as expected
+ *
  * @author Mislav Milicevic
  * @since 2019-08-16
  */
@@ -18,6 +21,26 @@ public final class Characters {
      * @return if the character is alphanumeric
      */
     public static boolean isAlphanumeric(char c) {
-        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9');
+        return isLetter(c) || isDigit(c);
+    }
+
+    /**
+     * Checks if the input character is a letter
+     *
+     * @param c input character
+     * @return if the number is a letter
+     */
+    public static boolean isLetter(char c) {
+        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+    }
+
+    /**
+     * Checks if the input character is a digit
+     *
+     * @param c input character
+     * @return if the character is a digit
+     */
+    public static boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
     }
 }
--- src/core/commons/src/test/java/com/nsoft/chiwava/core/commons/unit/CharactersTest.java
+++ src/core/commons/src/test/java/com/nsoft/chiwava/core/commons/unit/CharactersTest.java
@@ -25,4 +25,32 @@ public class CharactersTest {
     public void shouldNotBeAlphanumeric() {
         Assert.assertFalse(Characters.isAlphanumeric('.'));
     }
+
+    @Test
+    public void shouldBeLetter() {
+        for (char c = 'a'; c <= 'z'; c++) {
+            Assert.assertTrue(Characters.isLetter(c));
+        }
+
+        for (char c = 'A'; c <= 'Z'; c++) {
+            Assert.assertTrue(Characters.isLetter(c));
+        }
+    }
+
+    @Test
+    public void shouldNotBeLetter() {
+        Assert.assertFalse(Characters.isLetter('.'));
+    }
+
+    @Test
+    public void shouldBeDigit() {
+        for (char c = '0'; c <= '9'; c++) {
+            Assert.assertTrue(Characters.isDigit(c));
+        }
+    }
+
+    @Test
+    public void shouldNotBeDigit() {
+        Assert.assertFalse(Characters.isDigit('.'));
+    }
 }
