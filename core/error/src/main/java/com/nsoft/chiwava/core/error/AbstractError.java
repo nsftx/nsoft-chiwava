0adfc6f9921f8473f7341888d4fb8b015354a58a
Mislav Milicevic <milicevic.mislav@nsoft.com>
2019-09-06 14:35:08 +0200

    Implement error module
    Refs #4
    
    Signed-off-by: Mislav Milicevic <milicevic.mislav@nsoft.com>

---
+++ core/error/src/main/java/com/nsoft/chiwava/core/error/AbstractError.java
@@ -0,0 +1,10 @@
+package com.nsoft.chiwava.core.error;
+
+/**
+ * Abstract implementation of {@link Error}
+ *
+ * @author Mislav Milicevic
+ * @since 2019-09-06
+ */
+public abstract class AbstractError extends RuntimeException implements Error {
+}
