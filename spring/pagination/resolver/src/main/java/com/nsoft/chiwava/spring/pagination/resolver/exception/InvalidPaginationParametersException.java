639b65549bde49e7e63acfaa86bf8d202a4dfa8e
Mislav Milicevic <milicevic.mislav@nsoft.com>
2019-09-10 10:50:17 +0200

    Throw 400 if pagination request is invalid
    Closes #20

--- spring/pagination/resolver/src/main/java/com/nsoft/chiwava/spring/pagination/resolver/exception/InvalidPaginationParametersException.java
+++ spring/pagination/resolver/src/main/java/com/nsoft/chiwava/spring/pagination/resolver/exception/InvalidPaginationParametersException.java
@@ -1,11 +1,29 @@
 package com.nsoft.chiwava.spring.pagination.resolver.exception;
 
-public class InvalidPaginationParametersException extends RuntimeException {
-    public InvalidPaginationParametersException() {
-        super();
-    }
+import com.nsoft.chiwava.core.error.AbstractError;
+import com.nsoft.chiwava.core.error.Title;
+import org.zalando.problem.Status;
+
+public class InvalidPaginationParametersException extends AbstractError {
+
+    private static final Title EXCEPTION_TITLE = new Title() {
+        @Override
+        public String toString() {
+            return "exception.invalid_pagination_parameters";
+        }
+    };
 
     public InvalidPaginationParametersException(String details) {
         super(details);
+    }
+
+    @Override
+    public Status getStatus() {
+        return Status.BAD_REQUEST;
+    }
+
+    @Override
+    public Title getTitle() {
+        return EXCEPTION_TITLE;
     }
 }
