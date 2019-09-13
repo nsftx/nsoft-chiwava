d41ee0d7ddb3dd24dcc64bf4c2728ed4dc116854
Mislav Milicevic <milicevic.mislav@nsoft.com>
2019-09-13 09:42:09 +0200

    Add PersistenceTimestampable Javadoc example
    Refs #21

--- core/persistence/src/main/java/com/nsoft/chiwava/core/persistence/PersistenceTimestampable.java
+++ core/persistence/src/main/java/com/nsoft/chiwava/core/persistence/PersistenceTimestampable.java
@@ -9,6 +9,30 @@ import java.time.LocalDateTime;
  * {@link com.nsoft.chiwava.core.persistence.listener.PersistenceTimestampableListener} as an
  * argument to enable pre-persist timestamping
  *
+ * <pre>
+ *     &#64;Entity
+ *     &#64;EntityListeners(PersistenceTimestampableListener.class)
+ *     &#64;Getter
+ *     public class SimpleEntity implements PersistenceTimestampable {
+ *
+ *          &#64;Id
+ *          &#64;GeneratedValue(strategy = GenerationType.IDENTITY)
+ *          &#64;Column(name = "id", nullable = false, updatable = false)
+ *          private Long id;
+ *
+ *          &#64;Column(name = "created_at", nullable = false, updatable = false)
+ *          &#64;Setter
+ *          private LocalDateTime createdAt;
+ *
+ *          &#64;Column(name = "updated_at", nullable = false)
+ *          &#64;Setter
+ *          private LocalDateTime updatedAt;
+ *
+ *          public SimpleEntity() {
+ *          }
+ *     }
+ * </pre>
+ *
  * @author Mislav Milicevic
  * @since 2019-09-06
  */
