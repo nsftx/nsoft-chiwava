ded217e8c4de9c6814d5d0e1e4e72ed1edfcc57e
Mislav Milicevic <milicevic.mislav@nsoft.com>
2019-09-13 09:46:57 +0200

    Add UuidToStringConverter Javadoc example
    Refs #21

--- core/persistence/src/main/java/com/nsoft/chiwava/core/persistence/converters/UuidToStringConverter.java
+++ core/persistence/src/main/java/com/nsoft/chiwava/core/persistence/converters/UuidToStringConverter.java
@@ -6,6 +6,26 @@ import java.util.UUID;
 /**
  * Converts UUID properties inside entities to Strings
  *
+ * <pre>
+ *     &#64;Entity
+ *     &#64;Getter
+ *     public class SimpleEntity implements PersistenceTimestampable {
+ *
+ *          &#64;Id
+ *          &#64;GeneratedValue(strategy = GenerationType.IDENTITY)
+ *          &#64;Column(name = "id", nullable = false, updatable = false)
+ *          private Long id;
+ *
+ *          &#64;Column(name = "uuid", nullable = false, updatable = false)
+ *          &#64;Convert(converter = UuidToStringConverter.class)
+ *          private UUID uuid;
+ *
+ *          public SimpleEntity() {
+ *              this.uuid = UUID.randomUUID();
+ *          }
+ *     }
+ * </pre>
+ *
  * @author Mislav Milicevic
  * @since 2019-09-09
  */
